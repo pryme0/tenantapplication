66bf6bd4c1b14dd47626189dca241812ec078539
pryme0 <obochi2@gmail.com>
2022-10-11 09:34:50 +0100

    update app meta name

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-     <Head>
-        <meta name="description" content="Casabe tenant application" />
+      <Head>
+        <meta name="description" content="Casabe" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,800;0,900;1,100&display=swap"
           rel="stylesheet"
